07bd137d6df0f034fd374d533ce912b338a5fe85
Asfand Ijaz Malik <asfandijazmalik@gmail.com>
2024-11-15 22:35:36 +0500

    Delete 03_que_for_loop.py

--- 01_Easy/03_que_for_loop.py
+++
@@ -1,8 +0,0 @@
-# Print Even Number form 1 to 10 
-# Like 2 4 6 8 10
- 
-# with if statement 
-print(f'\nWith If Staement\n')
-for num in range(1, 11):
-    if num % 2 == 0:
-        print(num, end=' ')
