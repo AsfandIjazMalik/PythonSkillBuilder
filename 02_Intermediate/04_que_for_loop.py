e9f17ead9255f3565b9d4e9deafc4c8306916752
Asfand Ijaz Malik <asfandijazmalik@gmail.com>
2024-11-15 22:35:40 +0500

    Delete 04_que_for_loop.py

--- 02_Intermediate/04_que_for_loop.py
+++
@@ -1,9 +0,0 @@
-# Sum of number from 1 to 10
-# expected output will be : 55
-
-total = 0 
-for num in range(1, 11):
-    # additiona assignment operator 
-    total+= num # 1+2+3+4+5+6+7+8+9+10
-
-print(total)
