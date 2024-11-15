dc69d34873b560785d6118fb8b466c7e03fb28cf
Asfand Ijaz Malik <asfandijazmalik@gmail.com>
2024-11-15 22:37:36 +0500

    Create 07_que_while_loop.py

---
+++ PythonSkillBuilder/While Loop/02_Intermediate/07_que_while_loop.py
@@ -0,0 +1,10 @@
+# Print first five multiple of 4 
+# Expected output is : 4 8 12 16 20
+
+multiple_four = 4 
+count = 1
+
+while count <= 5:
+    print(f'{multiple_four}', end = ' ')
+    multiple_four += 4
+    count +=1
