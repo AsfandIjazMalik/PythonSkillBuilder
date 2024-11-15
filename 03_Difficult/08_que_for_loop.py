9251627f52225b04f4122ef7a53b50b8b47edb7b
Asfand Ijaz Malik <asfandijazmalik@gmail.com>
2024-11-15 22:36:01 +0500

    Delete 08_que_for_loop.py

--- 03_Difficult/08_que_for_loop.py
+++
@@ -1,8 +0,0 @@
-# Calculate Factorial of 5 
-# # Expected Output is : 120
-
-num_factorial = int(input('Enter the number for which you want to calculate the factorial: '))
-factorial = 1
-for i in range(num_factorial, 1 , -1):
-    factorial *= i
-print(f'Factorial of {num_factorial} is {factorial}')
