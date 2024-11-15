0d41481e2f0eafa8b8c50cbd9776ea667fb41616
Asfand Ijaz Malik <asfandijazmalik@gmail.com>
2024-11-15 22:37:30 +0500

    Create 05_que_while_loop.py

---
+++ PythonSkillBuilder/While Loop/02_Intermediate/05_que_while_loop.py
@@ -0,0 +1,9 @@
+# Reverse this namr dlroW wolleH
+# Expected Output : Hellow World
+
+sentence = 'dlroW wolleH'
+counter = len(sentence) - 1
+
+while counter >= 0:
+    print(sentence[counter], end='')
+    counter -= 1
