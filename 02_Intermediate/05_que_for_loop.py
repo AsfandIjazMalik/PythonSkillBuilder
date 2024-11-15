07d85d338661705356f3f66099ffff7e4216ac2b
Asfand Ijaz Malik <asfandijazmalik@gmail.com>
2024-11-15 22:35:43 +0500

    Delete 05_que_for_loop.py

--- 02_Intermediate/05_que_for_loop.py
+++
@@ -1,8 +0,0 @@
-# Revers this word 'kilaM zajI dnafsA'
-# Expected output is : Asfand Ijaz Mlaik
-
-# Reverse Indexing
-word = 'kilaM zajI dnafsA'
-# len(word) - 1 is used to get last character of the word
-for char in range(len(word) - 1, -1, -1): 
-    print(word[char], end='') 
